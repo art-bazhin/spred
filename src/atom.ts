5fd958da04cfdbface7565bd16120abc3a8aa443
art-bazhin <bazhin.artyom@yandex.ru>
2021-10-28 18:12:23 +0300

    fix set function return type

--- src/atom.ts
+++ src/atom.ts
@@ -9,19 +9,19 @@ const atomProto = {
   __proto__: observableProto,
   set(value: any) {
     commit([this as any,  value]);
+    return value;
   }
 }
 
 export interface Atom<T> extends Observable<T> {
   (value: T): T;
-  set(value: T): void;
+  set(value: T): T;
 }
 
 export function atom<T>(value: T) {
   const f = function (value?: T) {
     if (!arguments.length) return f.get();
-    f.set(value as T);
-    return value;
+    return f.set(value as T);
   } as Atom<T>;
 
   (f as any)[STATE_KEY] = createState(value);
