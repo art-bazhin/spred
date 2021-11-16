2424dd1f0ed498e6aa8e6e2900a20154fc2011d6
art-bazhin <bazhin.artyom@yandex.ru>
2021-11-16 19:01:45 +0300

    feat: add store options

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,11 +2,20 @@ import { writable, WritableAtom } from '../writable/writable';
 import { Atom } from '../atom/atom';
 import { computed } from '../computed/computed';
 
+interface StoreOptions<T> {
+  getItemId?: (item: T) => string;
+  shouldUpdate?: (value: T | undefined, prevValue: T | undefined) => boolean;
+}
+
+interface StoreOptionsWithId<T> extends StoreOptions<T> {
+  getItemId: (item: T) => string;
+}
+
 interface StoreData<T> {
   [id: string]: T | undefined;
 }
 
-interface Store<T> {
+export interface Store<T> {
   get(id: string): Atom<T | undefined>;
   set(...items: T[]): void;
   delete(id: string): void;
@@ -14,6 +23,7 @@ interface Store<T> {
 }
 
 interface _Store<T> extends Store<T> {
+  _options: StoreOptions<T>;
   _idFn: (item: T) => string;
   _data: WritableAtom<StoreData<T>>;
   _atoms: {
@@ -21,24 +31,29 @@ interface _Store<T> extends Store<T> {
   };
 }
 
-function defaultIdFn<T>(item: T) {
-  return (item as any).id;
-}
+const DEFAULT_STORE_OPTIONS = {
+  getItemId: (item: any) => item.id,
+};
 
-function createData<T>(items: T[], idFunction = defaultIdFn) {
+function createData<T>(items: T[], getItemId: (item: T) => string) {
   const data: StoreData<T> = {};
 
   if (!items) return data;
 
   for (let item of items) {
-    data[idFunction(item)] = item;
+    data[getItemId(item)] = item;
   }
 
   return data;
 }
 
 function get<T>(this: _Store<T>, id: string) {
-  if (!this._atoms[id]) this._atoms[id] = computed(() => this._data()[id]);
+  if (!this._atoms[id]) {
+    this._atoms[id] = computed<T | undefined>(() => this._data()[id], {
+      shouldUpdate: this._options.shouldUpdate,
+    });
+  }
+
   return this._atoms[id];
 }
 
@@ -62,15 +77,21 @@ function clear<T>(this: _Store<T>) {
   this._data({});
 }
 
-export function store<T extends { id: string }>(items?: T[]): Store<T>;
+export function store<T extends { id: string }>(
+  items?: T[],
+  options?: StoreOptions<T>
+): Store<T>;
 
-export function store<T>(items: T[], idFunction: (item: T) => string): Store<T>;
+export function store<T>(items: T[], options: StoreOptionsWithId<T>): Store<T>;
 
-export function store<T>(items?: any, idFunction?: any) {
+export function store<T>(items?: any, options?: any) {
+  const opts = Object.assign({}, DEFAULT_STORE_OPTIONS, options || {});
+
   const res: _Store<T> = {
-    _idFn: idFunction || defaultIdFn,
+    _options: opts,
+    _idFn: opts.getItemId,
     _atoms: {},
-    _data: writable(createData(items, idFunction)),
+    _data: writable(createData(items, opts.getItemId)),
     get,
     set,
     clear,
