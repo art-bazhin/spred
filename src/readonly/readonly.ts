611ab4bcbc9e2da5e01f839c9602d10c0cd817b7
art-bazhin <bazhin.artyom@yandex.ru>
2021-12-08 19:48:03 +0300

    docs(readonly): add readonly function docs

--- src/readonly/readonly.ts
+++ src/readonly/readonly.ts
@@ -2,6 +2,10 @@ import { computed } from '../computed/computed';
 import { TRUE } from '../utils/functions';
 import { WritableAtom } from '../writable/writable';
 
+/**
+ * Creates a readonly copy of the writable atom.
+ * @param atom Source atom.
+ */
 export function readonly<T>(atom: WritableAtom<T>) {
   return computed(() => atom(), null, TRUE);
 }
