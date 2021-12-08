91f8de7c755cc69c7959ce523d0d6b7cfdebf273
art-bazhin <bazhin.artyom@yandex.ru>
2021-12-08 19:32:40 +0300

    docs(computed): add computed function docs

--- src/computed/computed.ts
+++ src/computed/computed.ts
@@ -4,8 +4,14 @@ import { Filter } from '../filter/filter';
 import { createState } from '../state/state';
 import { VOID } from '../void/void';
 
+/**
+ * Creates an atom that automatically calculates its value from other atoms
+ * @param computedFn The function that calculates atom value and returns it.
+ * @param catchException A function that handles an exception thrown when calculating the atom and returns the new atom value.
+ * @param shouldUpdate A function that takes the new and current atom value, and returns true if the atom value needs to be updated.
+ */
 export function computed<T>(
-  computedFn: (currentValue?: T) => T,
+  computedFn: (currentValue: T | VOID) => T,
   catchException?: ((e: unknown, cuurentValue?: T) => T) | null,
   shouldUpdate?: Filter<T>
 ) {
