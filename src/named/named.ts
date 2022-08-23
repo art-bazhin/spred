ecb6bb139b8df791b91b7b86743ceb846ac1362d
art-bazhin <bazhin.artyom@yandex.ru>
2022-08-23 19:24:47 +0300

    fix(named): fix typing

--- src/named/named.ts
+++ src/named/named.ts
@@ -1,6 +1,9 @@
 import { Signal } from '../signal/signal';
 
-export function named<T>(signal: Signal<T>, name?: string | null | false) {
+export function named<T, S extends Signal<T>>(
+  signal: S,
+  name?: string | null | false
+) {
   (signal as any)._state.name = name;
   return signal;
 }
