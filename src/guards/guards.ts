ee611916f66a3948b56e563a7d3fa1af6eb99817
art-bazhin <bazhin.artyom@yandex.ru>
2024-01-22 11:18:11 +0300

    docs(guards): add docs

--- src/guards/guards.ts
+++ src/guards/guards.ts
@@ -1,11 +1,21 @@
 import { Signal, WritableSignal } from '../core/core';
 
+/**
+ * Checks if the passed value is a {@link Signal}
+ * @param value A value to check.
+ * @returns True if the value is a {@link Signal}, false otherwise.
+ */
 export function isSignal(value: unknown): value is Signal<unknown>;
 
 export function isSignal(value: any) {
   return value instanceof Signal;
 }
 
+/**
+ * Checks if the passed value is a {@link WritableSignal}
+ * @param value A value to check.
+ * @returns True if the value is a {@link WritableSignal}, false otherwise.
+ */
 export function isWritableSignal(
   value: unknown,
 ): value is WritableSignal<unknown>;
