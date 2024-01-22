4d2853b616187c6decb98db109bdad5f4cba21c6
art-bazhin <bazhin.artyom@yandex.ru>
2024-01-22 11:35:08 +0300

    docs(config): add docs

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,7 @@
+/**
+ * A library configuration object.
+ * @property {Config} logException A function that logs exceptions. Default is console.error.
+ */
 export interface Config {
   logException: (e: unknown) => any;
 }
@@ -8,6 +12,10 @@ const DEFAULT_CONFIG: any = {
 
 export const config = Object.assign({}, DEFAULT_CONFIG);
 
+/**
+ * Configurates the library. Call without arguments to use the default configuration.
+ * @param configUpdate A configuration object.
+ */
 export function configure(configUpdate?: Partial<Config>) {
   Object.assign(config, configUpdate || DEFAULT_CONFIG);
 }
