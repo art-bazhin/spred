18cb4c3573e6e21780484f176c8ed93146a6b96e
art-bazhin <bazhin.artyom@yandex.ru>
2024-11-03 14:59:12 +0300

    style: use export from in index file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { Signal, WritableSignal, Operator } from './core/core';
-import { configure, Config } from './config/config';
-import {
+export { Signal, WritableSignal, Operator } from './core/core';
+export { configure, Config } from './config/config';
+export {
   batch,
   action,
   collect,
@@ -9,24 +9,6 @@ import {
   Computation,
   TrackingGetter,
 } from './core/core';
-import { on } from './on/on';
-import { signal } from './signal/signal';
-import { effect } from './effect/effect';
-
-export {
-  Signal,
-  WritableSignal,
-  SignalOptions,
-  Config,
-  Subscriber,
-  Operator,
-  Computation,
-  TrackingGetter,
-  signal,
-  batch,
-  action,
-  on,
-  effect,
-  configure,
-  collect,
-};
+export { on } from './on/on';
+export { signal } from './signal/signal';
+export { effect } from './effect/effect';
