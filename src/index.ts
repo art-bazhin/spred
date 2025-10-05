643fa4f928f9d2e18f9a249e7e38c725115ff30b
art-bazhin <bazhin.artyom@yandex.ru>
2025-10-05 19:33:28 +0300

    add size-limit

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
 export { Signal, WritableSignal, Operator } from './core/core';
-export { configure, Config } from './config/config';
+// export { configure, Config } from './config/config';
 export {
-  batch,
-  action,
-  collect,
+  // batch,
+  // action,
+  // collect,
   Subscriber,
   SignalOptions,
   WritableSignalOptions,
   Computation,
   TrackingGetter,
-  NONE,
+  // NONE,
 } from './core/core';
-export { on } from './on/on';
-export { signal } from './signal/signal';
-export { effect } from './effect/effect';
+// export { on } from './on/on';
+// export { signal } from './signal/signal';
+// export { effect } from './effect/effect';
